09f0489780d8b30cdeccd1764848d1707403d9ff
dcy <dcy10000@gmail.com>
2022-07-26 14:32:57 +0800

    version

---
+++ cmake-gpu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -x
+source ~/intel/oneapi/setvars.sh intel64
+source ~/miniconda3/etc/profile.d/conda.sh  &&rm -rf build && cmake -DFAISS_OPT_LEVEL=avx2  -DFAISS_ENABLE_GPU=ON -DCMAKE_BUILD_TYPE=Release -B build .
