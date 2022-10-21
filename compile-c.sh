1e2f3bdec94902bc507284d5ffdd209ff1579d96
dcy10000 <dcy10000@gmail.com>
2022-10-21 14:49:27 +0800

    modify compile script

--- compile-c.sh
+++ compile-c.sh
@@ -5,5 +5,9 @@ export LD_LIBRARY_PATH=/usr/local/cuda-11.7/lib64:$LD_LIBRARY_PATH
 #export LD_PRELOAD=/home/dcy/intel/oneapi/mkl/latest/lib/intel64/libmkl_def.so.2:/home/dcy/intel/oneapi/mkl/latest/lib/intel64/libmkl_avx2.so.2:/home/dcy/intel/oneapi/mkl/latest/lib/intel64/libmkl_core.so.2:/home/dcy/intel/oneapi/mkl/latest/lib/intel64/libmkl_intel_lp64.so.2:/home/dcy/intel/oneapi/mkl/latest/lib/intel64/libmkl_intel_thread.so.2:/home/dcy/intel/oneapi/compiler/latest/linux/compiler/lib/intel64_lin/libiomp5.so:/home/dcy/intel/oneapi/mkl/latest/lib/intel64/libmkl_sequential.so.2
 source ~/miniconda3/etc/profile.d/conda.sh && echo "$LD_LIBRARY_PATH" &&\
 make -C build -j faiss_avx2 \
- && make -C build -j $1 \
+ &&(
+  if [ -n "$1" ];then
+     make -C build -j $1 \
  && ./build/demos/$1
+  fi
+     )
